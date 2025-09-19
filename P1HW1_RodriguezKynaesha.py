65808c581100f268fc0dcb1a0c59107551c02261
rodriguk-skool <rodriguk4816@student.faytechcc.edu>
2025-09-18 20:45:28 -0400

    Add files via upload

---
+++ P1HW1_RodriguezKynaesha.py
@@ -0,0 +1,15 @@
+# Kynaesha Rodriguez
+# September 18, 2025
+# P1HW1
+# Simple program for exponents, addition, and subtraction
+
+print("-----Calculating Exponents----")
+base = int(input("Enter an integer as the base value: "))
+exp = int(input("Enter an integer as the exponent: "))
+print(f"\n{base} raised to the power of {exp} is {base ** exp} !!")
+
+print("\n-----Addition and Subtraction----")
+a = int(input("Enter a starting integer: "))
+b = int(input("Enter an integer to add: "))
+c = int(input("Enter an integer to subtract: "))
+print(f"\n{a} + {b} - {c} is equal to {a + b - c}")
